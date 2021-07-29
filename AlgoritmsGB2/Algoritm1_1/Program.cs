e96f7c632e618230c35444657568bb776da02873
Balfegoru <duccoducco@gmail.com>
2021-07-29 16:55:44 +0300

    Домашняя работа 1 (задания 1 и 3)

---
+++ AlgoritmsGB2/Algoritm1_1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm1_1
+{
+    class Program
+    {
+        //Напишите на C# функцию согласно блок-схеме
+        
+        
+
+        //Класс для теста
+        public class TestCase
+        {
+            public int number { get; set; }
+            public string Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        //Метод теста
+        static void TestSimpNot(TestCase testCase)
+        {
+            try
+            {
+                var actual = SimpleNot(testCase.number);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+
+            }
+        }
+
+        //Метод определение простоты числа
+        static string SimpleNot(int number)
+        {
+            int d = 0, i = 2;
+
+            while (i < number)
+            {
+                if(number%i == 0)
+                {
+                    d++;
+                }
+
+                i++;
+            }
+
+            if (d == 0)
+            {
+                
+                return "Простое";
+            }
+            else
+            {
+                return "Не простое";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Ввод данных
+            var testCase1 = new TestCase()
+            {
+                number = 5,
+                Expected = "Простое",
+                ExpectedException = null
+            };
+
+            var testCase2 = new TestCase()
+            {
+                number = 101,
+                Expected = "Простое",
+                ExpectedException = null
+            };
+
+
+            var testCase3 = new TestCase()
+            {
+                number = 3211,
+                Expected = "Не простое",
+                ExpectedException = null
+            };
+
+            //Проверка
+            TestSimpNot(testCase1);
+            TestSimpNot(testCase2);
+            TestSimpNot(testCase3);
+        }
+    }
+}
---
+++ AlgoritmsGB2/Fibonachi1_2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi1_2
+{
+    class Program
+    {
+
+        //Требуется реализовать
+        //рекурсивную версию и версию без рекурсии (через цикл).
+
+
+        //Класс для теста
+        public class TestCase
+        {
+            public int number { get; set; }
+            public int Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        //Проверка рекурсии
+        static void TestFibRecurs(TestCase testCase)
+        {
+            try
+            {
+                var actual = RecursFib(testCase.number);
+                
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+
+            }
+        }
+
+        //Проверка цикла
+        static void TestFibCicle(TestCase testCase)
+        {
+            try
+            {
+                var actual = CicleFib(testCase.number);
+
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+
+            }
+        }
+
+        //Рекурсия
+        static int RecursFib(int number)
+        {
+            if(number==1 )
+            {
+                return 0;
+            }
+            else if (number == 2) { return 1; }
+
+            return RecursFib(number - 2) + RecursFib(number - 1);
+        }
+
+        //Цикл
+        static int CicleFib(int number)
+        {
+            if (number == 1)
+            {
+                return 0;
+            }
+            else if (number == 2) { return 1; }
+
+            int i = 2, first = 0, second = 1, result;
+
+            do
+            {
+                result = first + second;
+                first = second;
+                second = result;
+                i++;
+            } while (i < number);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            var testCase1 = new TestCase()
+            {
+                number = 1,
+                Expected = 0,
+                ExpectedException = null
+            };
+
+            var testCase2 = new TestCase()
+            {
+                number = 2,
+                Expected = 1,
+                ExpectedException = null
+            };
+
+            var testCase3 = new TestCase()
+            {
+                number = 11,
+                Expected = 55,
+                ExpectedException = null
+            };
+
+
+            TestFibRecurs(testCase1);
+            TestFibRecurs(testCase2);
+            TestFibRecurs(testCase3);
+
+            TestFibCicle(testCase1);
+            TestFibCicle(testCase2);
+            TestFibCicle(testCase3);
+
+        }
+    }
+}
